049e3b7cc52a41bf693e22867e830d7fd63762b6
Ivan Brosalin <iebrosalin@gmail.com>
2025-05-26 14:41:37 +0700

    vault backup: 2025-05-26 14:41:37

--- BSCP/3. Authentication vulnerabilities/14. Brute-forcing a stay-logged-in cookie/gen_md5.py
+++ BSCP/3. Authentication vulnerabilities/14. Brute-forcing a stay-logged-in cookie/gen_md5.py
@@ -3,5 +3,4 @@ import hashlib
 with open('pass_list.txt', 'r', encoding='UTF-8') as file_input_pass:
     with open('output_pass_md5.txt', 'a', encoding='UTF-8') as file_output_pass:
         while line := file_input_pass.readline():
-            file_output_pass.write(line)
-            file_output_pass.write(hashlib.md5(str.strip(line).encode).hexdigest())
+            file_output_pass.write(hashlib.md5(str(line).encode).hexdigest())
